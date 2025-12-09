1c624f8904bdf10fc4897b72d85c94a3b39bbc49
Abudu Abidun Sulaiman <sulaimanabiodun172@gmail.com>
2025-12-09 11:56:40 +0100

    add port env variable

--- src/server.ts
+++ src/server.ts
@@ -29,8 +29,16 @@ const start = async () => {
   })
 
   server.use(payload.authenticate);
+  
+  const port = Number(process.env.PORT) || 3000;
 
-  server.listen({ port: 3000 });
+  server.listen({ port, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+    server.log.info(`Server listening at ${address}`);
+  });
 }
 
 start();
